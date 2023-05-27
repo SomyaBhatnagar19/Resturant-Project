88c0a4e14f87055b9312727dd9f0e8c8d5daae9e
SomyaBhatnagar19 <somya.bhatnagar98@gmail.com>
2023-05-27 12:19:27 +0530

    Adding the Cart button component.

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,24 @@
+import React, { Fragment } from "react";
+
+import classes from "./Header.module.css";
+//if we are using image as part of the softwware as a local image but not url then we import it
+import mealsImage from "../../assets/meals.jpg";
+
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>Happy Meals</h1>
+        <HeaderCartButton/>
+      </header>
+      <div className={classes["main-image"]}>
+        {/* locally calling image here */}
+        <img src={mealsImage} alt="A table full of delicious food!" />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
